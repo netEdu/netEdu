52cf5c9bef3a169e0d9528daecfbe102e5d39904
liuzizheng <liuzizheng@91isoft.com>
2018-04-19 10:41:40 +0800

    更新课程

---
+++ src/main/java/com/netEdu/lesson/apply/controller/CourseController.java
@@ -0,0 +1,62 @@
+package com.netEdu.lesson.apply.controller;
+
+import com.netEdu.entity.Course;
+import com.netEdu.lesson.apply.service.CourseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Course")
+@Api(description = "|教师端|课程申请")
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @ApiOperation(value = "|Course|申请课程",notes = "course_name:课程名称</br>" +
+            "credit:学分</br>" +
+            "hours:课时</br>" +
+            "teacher_id:教师id</br>" +
+            "class_time:学时</br>" +
+            "course_introduce:课程简介</br>" +
+            "class_num:班号</br>" +
+            "assessment_method:考核方法")
+    @PostMapping(value = "/applyCourse")
+    public String add(@RequestBody Course course){
+        return courseService.add(course);
+    }
+
+    @ApiOperation(value = "|Course|修改申请",notes = "course_id:课程id" +
+            "course_name:课程名称</br>" +
+            "credit:学分</br>" +
+            "hours:课时</br>" +
+            "class_time:学时</br>" +
+            "course_introduce:课程简介</br>" +
+            "class_num:班号</br>" +
+            "assessment_method:考核方法")
+    @PutMapping(value = "/updateApplication")
+    public void update(@RequestBody Course course){
+        courseService.updateCourse(course);
+    }
+
+    @ApiOperation(value = "|Course|取消申请",notes = "ids:课程id组 split by ','")
+    @DeleteMapping(value = "/cancelCourse")
+    public void del(@RequestParam String ids){
+        courseService.remove(ids);
+    }
+
+    @ApiOperation(value = "|Course|查看申请列表",notes = "teacher_id:教师id</br>" +
+            "course_name:课程名称 模糊</br>" +
+            "course_introduce:课程简介 模糊</br>" +
+            "class_num:班号</br>" +
+            "audit_status:审核状态 0:待审 1:通过 2:不行")
+    @PostMapping(value = "/courseList")
+    public List<Course> query(@RequestBody Course course){
+        return courseService.selectCourse(course);
+    }
+
+}
---
+++ src/main/java/com/netEdu/lesson/apply/service/impl/CourseImpl.java
@@ -0,0 +1,49 @@
+package com.netEdu.lesson.apply.service.impl;
+
+import com.netEdu.entity.Course;
+import com.netEdu.lesson.apply.dao.CourseMapper;
+import com.netEdu.lesson.apply.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CourseImpl implements CourseService {
+
+    @Autowired
+    private CourseMapper courseMapper;
+
+    @Override
+    public String add(Course course) {
+        if(courseMapper.queryCourse(course.getCourse_name()).size() > 0){
+            return "REPEAD";
+        }
+        else{
+            Date date=new Date();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            course.setCreate_time(df.format(date));
+            course.setAudit_status("0");
+            courseMapper.insertSelective(course);
+        }
+        return "SUCCESS";
+    }
+
+    @Override
+    public void updateCourse(Course course) {
+        courseMapper.updateByPrimaryKeySelective(course);
+    }
+
+    @Override
+    public void remove(String ids) {
+        courseMapper.deleteCourse(ids);
+    }
+
+    @Override
+    public List<Course> selectCourse(Course course) {
+        return courseMapper.queryCourseList(course);
+    }
+
+}
