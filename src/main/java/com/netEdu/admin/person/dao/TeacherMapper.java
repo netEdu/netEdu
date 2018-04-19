6b5e34e970bba493238f303fbc97c682be33fb9b
liuzizheng <liuzizheng@91isoft.com>
2018-04-19 14:58:54 +0800

    Revert "更新课程，资料上传部分可以查看可用课程"
                               This reverts commit a12d323

---
+++ src/main/java/com/netEdu/admin/person/dao/TeacherMapper.java
@@ -0,0 +1,9 @@
+package com.netEdu.admin.person.dao;
+
+import com.netEdu.core.BaseMapper;
+import com.netEdu.entity.Teacher;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TeacherMapper extends BaseMapper<Teacher>{
+}
--- src/main/java/com/netEdu/admin/person/service/PersonService.java
+++ src/main/java/com/netEdu/admin/person/service/PersonService.java
@@ -1,4 +1,12 @@
 package com.netEdu.admin.person.service;
 
+import com.netEdu.entity.Student;
+import com.netEdu.entity.Teacher;
+
 public interface PersonService {
+
+    void addTeacher(Teacher teacher);
+
+    void addStudent(Student student);
+
 }
