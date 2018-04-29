ef91cf77cc6959708d3cfc676432ecb5f9281e8c
rekobi <352059284@qq.com>
2018-04-30 02:10:27 +0800

    修正了分组管理的bad sql，增添了在登录时绑定已加入的聊天组

---
+++ src/main/java/com/netEdu/utils/netty/GroupInjectMapper.java
@@ -0,0 +1,20 @@
+package com.netEdu.utils.netty;
+
+import com.netEdu.entity.Group;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface GroupInjectMapper {
+
+    @Select("select group_id from `group` where person_id like concat('%' , ',' , #{id} , ',' , '%'")
+    @Results({
+            @Result(property = "group_id",  column = "group_id")
+    })
+    List<Group> getAllGroup(String id);
+
+}
---
+++ src/main/java/com/netEdu/utils/netty/GroupInjectService.java
@@ -0,0 +1,45 @@
+package com.netEdu.utils.netty;
+
+import com.netEdu.entity.Group;
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GroupInjectService {
+
+    @Autowired GroupInjectMapper groupInjectMapper;
+
+    void injectGroups(String id){
+
+        List<Group> groupList=groupInjectMapper.getAllGroup(id);
+        if (groupList.size()>0){
+            String gid="";
+            Channel channel;
+            for (Group group:groupList){
+                //获取组id
+                gid+=group.getGroup_id();
+                //通过人员id获取channel
+                channel=Connection.AllConnections.get(id);
+                //检测是否存在讨论组,不存在则创建并添加链接，否则取出channelGroup并添加
+                if(!Connection.chatGroup.containsKey(gid)){
+                    ChannelGroup channelGroup=new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+                    channelGroup.add(channel);
+                    Connection.chatGroup.put(gid,channelGroup);
+                }else {
+                    ChannelGroup channelGroup=Connection.chatGroup.get(gid);
+                    channelGroup.add(channel);
+                    Connection.chatGroup.put(gid,channelGroup);
+                }
+            }
+        }
+
+
+    }
+
+}
