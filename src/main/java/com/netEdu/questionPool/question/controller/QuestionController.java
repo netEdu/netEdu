e0ff75494210f73291df9689a30218975855c492
ChenLei0504 <13820467739@163.com>
2018-05-06 13:49:43 +0800

    更改接受参数方式

--- src/main/java/com/netEdu/questionPool/question/controller/QuestionController.java
+++ src/main/java/com/netEdu/questionPool/question/controller/QuestionController.java
@@ -84,7 +84,7 @@ public class QuestionController extends BaseController<Question> {
 
     @ApiOperation(value = "|TeacherEvaluate|查询未编排的问题")
     @PostMapping(consumes = APPLICATION_JSON_UTF8_VALUE,value = "/selectNotExistQuestion")
-    public ResponseMessage<List<Question>> selectNotExistQuestion(@RequestParam String existIds) throws Exception {
+    public ResponseMessage<List<Question>> selectNotExistQuestion(@RequestBody String existIds) throws Exception {
         String[] existQuestionIds=existIds.split(",");
         List existQuestionIdList= Arrays.asList(existQuestionIds);
         return Result.success(questionService.selectNotExistQuestion(existQuestionIdList));
