e18f1ffa812a747a17785cf758333e03e2651674
panxinyang <2251091259@qq.com>
2018-06-01 14:22:05 +0800

    添加全部同学的平均成绩查询

--- src/main/java/com/netEdu/exam/check/dao/ScoreMapper.java
+++ src/main/java/com/netEdu/exam/check/dao/ScoreMapper.java
@@ -5,6 +5,7 @@ import com.netEdu.entity.Score;
 import com.netEdu.entity.VO.ScoreVO;
 import com.netEdu.exam.check.page.ScorePage;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -45,4 +46,21 @@ public interface ScoreMapper extends BaseMapper<Score>{
             "WHERE score.student_id = #{0} GROUP BY score.student_id")
     ScoreVO AVGStudentId(int student_id);
 
+    //所有学生的平均成绩
+    @Select("<script>SELECT AVG(score.paper_score) AS paper_score ," +
+            "AVG(score.sign_score) AS sign_score, " +
+            "AVG(score.test_score) AS test_score, " +
+            "AVG(student_data.backup) AS data_score , " +
+            " student.`name` AS student_name " +
+            "FROM score " +
+            "left join student_data " +
+            "on score.student_id = student_data.student_id " +
+            " LEFT JOIN student " +
+            "ON student.student_id = score.student_id " +
+            "where 1=1" +
+            "<if test=\"class_num !=0 \"> and student.class_num = #{class_num}</if> " +
+            "<if test=\"student_name !=null and student_name != '' \"> and student.`name` LIKE concat('%',#{student_name},'%')</if> " +
+            "<if test=\"student_id !=0 \"> and student.student_id = #{student_id}</if> "+
+            " GROUP BY score.student_id</script>")
+    List<ScoreVO> AVGAllStudentId(ScorePage classRequirement);
 }
