285c89ada7776d6ee960aca2c1ddae935a0396f8
panxinyang <2251091259@qq.com>
2018-06-04 16:19:49 +0800

    修改bug

--- src/main/java/com/netEdu/admin/person/student/dao/StudentMapper.java
+++ src/main/java/com/netEdu/admin/person/student/dao/StudentMapper.java
@@ -18,13 +18,13 @@ public interface StudentMapper extends BaseMapper<Student>{
      * @return
      */
     @Select("<script>select count(1) from student " +
-            "<if test=\"name !=null and name != '' \">and name like CONCAT(CONCAT('%',#{name},'%')) </if> " +
+            "<if test=\"'name' !=null and 'name' != '' \">and 'name' like CONCAT(CONCAT('%',#{name},'%')) </if> " +
             "<if test=\"sex !=null and sex != '' \">and sex = #{sex} </if> " +
             "<if test=\"class_num !=null and class_num != '' \">and class_num = #{class_num} </if> </script> " )
     Integer queryByCount(StudentPage studentPage);
 
     @Select("<script>select * from student where 1=1 " +
-            "<if test=\"name !=null and name != '' \">and name like CONCAT(CONCAT('%',#{name},'%')) </if> " +
+            "<if test=\"'name' !=null and 'name' != '' \">and 'name' like CONCAT(CONCAT('%',#{name},'%')) </if> " +
             "<if test=\"sex !=null and sex != '' \">and sex = #{sex} </if> " +
             "<if test=\"class_num !=null and class_num != '' \">and class_num = #{class_num} </if> " +
             "and del_flag = 0 limit #{page},#{pageSize}" +
